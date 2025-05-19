409e9062bd2bdb93750c13f26889315ef53607c7
StockOslo <erik.antonov@icloud.com>
2025-05-20 00:17:00 +0300

    update changes

--- BottleWebProject_C224_6_AAD/app.py
+++ BottleWebProject_C224_6_AAD/app.py
@@ -5,7 +5,7 @@ This script runs the application using a development server.
 import bottle
 import os
 import sys
-from forms import recommendation_system_form, girvan_newman_solver, EducationalTrajectory, saveResult
+from forms import CreatingRecommendationSystemForm, girvan_newman_solver, EducationalTrajectory
 
 # routes contains the HTTP handlers for our server and must be imported.
 import routes
