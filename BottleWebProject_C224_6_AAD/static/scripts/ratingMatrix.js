96eb18e3b58bb956d33061545b13d7e4be195f8f
Svetik <sveta_dem06@mail.ru>
2025-05-19 13:35:49 +0300

    Checks are made for input of matrix row and column values.

--- BottleWebProject_C224_6_AAD/static/scripts/ratingMatrix.js
+++ BottleWebProject_C224_6_AAD/static/scripts/ratingMatrix.js
@@ -1,21 +1,21 @@
 ﻿function generateMatrix() {
-    const m = parseInt(document.getElementById('numUsers').value);  // rows
-    const n = parseInt(document.getElementById('numItems').value);  // columns
+    const m = Number(document.getElementById('numUsers').value);  // rows
+    const n = Number(document.getElementById('numItems').value);  // columns
 
     const errorM = document.getElementById('numUsersError');
     const errorN = document.getElementById('numItemsError');
 
     let valid = true;
 
-    if (isNaN(m) || m < 1 || m > 10) {
-        errorM.innerText = "Please enter a number between 1 and 10";
+    if (!Number.isInteger(m) || m < 3 || m > 10) {
+        errorM.innerText = "Please enter a number between 3 and 10";
         valid = false;
     } else {
         errorM.innerText = "";
     }
 
-    if (isNaN(n) || n < 1 || n > 10) {
-        errorN.innerText = "Please enter a number between 1 and 10";
+    if (!Number.isInteger(n) || n < 2 || n > 10) {
+        errorN.innerText = "Please enter a number between 2 and 10";
         valid = false;
     } else {
         errorN.innerText = "";
